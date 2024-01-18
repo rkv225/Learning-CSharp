cc9f19ba4fa4562a4a0ab1141d12793b44b0462b
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-01-18 19:50:52 +0530

    first program

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿// C# top level statements C# 9.0
+
+using System;
+
+int x = 2 + 3;
+Console.WriteLine(x);
+
+int a = int.MaxValue;
+int b = 1;
+int uc = unchecked (a + b);
+Console.WriteLine(uc); // this doesn't throw exception but gives out wrong value
+int c = checked (a + b); // Checks just the expression. // this throws exception
+Console.WriteLine(c);
+
