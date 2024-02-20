f8a4ac49712b39ddaa770a104e77773028700ee8
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-02-20 22:30:11 +0530

    LFU cache

--- csharp/records/Program.cs
+++ csharp/records/Program.cs
@@ -1,58 +1,29 @@
-﻿
-// records introduced in C# 9 to work well with immutable(readonly) data
-// best suited for data where we want to preserve immutability
-// Records are purely a C# compile-time construct
-// records support non-destructive mutation In order to “modify” an immutable object, you must create a new one and copy over the data while incorporating your modifications
-// Records give you structural equality by default. Structural equality means that two instances are the same if their data is the same (as with tuples).
+﻿using System.Data.Common;
 
-using System.Data;
-using System.Runtime;
+var a = new MyClass(1);
+var b = new MyClass(1);
+Console.WriteLine(a == b);
+Console.WriteLine(a.Equals(b));
 
-record RPoint
+class MyClass
 {
-    public RPoint (double x, double y) => (X, Y) = (x, y);
-    public double X { get; init; }
-    public double Y { get; init; }  
-}
-
-// creating a record does additional tasks
-// 1. it creates a protected copy constructor and a hidden clone method to support non destructive mutation
-// 2. it iverrides/overloads equality operators/functions
-// 3. it overrides ToString() method to print the definition of the record
-
-// A record definition can also include a parameter list:
-record Point (double X, double Y);
-record Test (int A, int B, int C, int D, int E, int F, int G, int H);
-/*
- If a parameter list is specified, the compiler performs the following extra steps:
-• It writes an init-only property per parameter.
-• It writes a primary constructor to populate the properties.
-• It writes a deconstru(ctor.
-*/
-
-// record types also support inheritance
-abstract record Icecream (string flavor, string topping);
-
-record IcecreamServing(string flavor, string topping, string size):Icecream(flavor, topping);
-
-class Program
-{
-    public static void Main(string[] args)
+    public int val { get; set; }
+    public MyClass(int x)
     {
-        Point p1 = new Point (3, 3);
-        Point p2 = p1 with { Y = 4 };
-        Console.WriteLine (p2); // Point { X = 3, Y = 4 }
-
-        Test t1 = new Test (1, 2, 3, 4, 5, 6, 7, 8);
-        Test t2 = t1 with { A = 10, C = 30 };
-        Console.WriteLine (t2);
-
-        // equality comparison
-        Point px = new (5, 2);
-        Point py = new (5, 2);
-        Console.WriteLine (px.Equals (py)); // True
-
-        IcecreamServing ic = new("chocolate", "almonds", "medium");
-        Console.WriteLine(ic);
+        val = x;
     }
+    public override int GetHashCode()
+    {
+        return val;
+    }
+    /*
+    public override bool Equals(object? obj)
+    {
+        if(obj is null) return false;
+        int x = ((MyClass)obj).val;
+        return val == x;
+    }
+    */
+    public static bool operator == (MyClass x, MyClass y) => x.val == y.val;
+    public static bool operator != (MyClass x, MyClass y) => x.val != y.val; 
 }
---
+++ os_exe/LFUCache/Program.cs
@@ -0,0 +1,136 @@
+﻿/*
+Problem:
+Design and implement a data structure for a Least Frequently Used (LFU) cache.
+
+Implement the LFUCache class:
+LFUCache(int capacity) Initializes the object with the capacity of the data structure.
+int get(int key) Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
+void put(int key, int value) Update the value of the key if present, or inserts the key if not already present. When the cache reaches its capacity, it should invalidate and remove the least frequently used key before inserting a new item. For this problem, when there is a tie (i.e., two or more keys with the same frequency), the least recently used key would be invalidated.
+To determine the least frequently used key, a use counter is maintained for each key in the cache. The key with the smallest use counter is the least frequently used key.
+When a key is first inserted into the cache, its use counter is set to 1 (due to the put operation). The use counter for a key in the cache is incremented either a get or put operation is called on it.
+The functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+
+Input
+["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+                 // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+                 // cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+                 // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+                 // cache=[4,3], cnt(4)=2, cnt(3)=3
+
+Solution:
+Keep element key, value and frequency  in a dictionary.
+Keep a frequency dictionary to with frequency as key and a list of values with the frequency in it's value.
+If value is present we remove it from freq map and reinsert it after incrementing the frequency
+if the capacity is full, we get the first value from freq map and then remove the first element as it will be the least frequently used.
+
+Complexity: As it not possible to solve this in O(1). Sorted dictionary has O(log n) complexity and a list remove works in O(capacity).
+So we can assume that it totally depends on whether log n or capcity of individual frequency
+
+Link: https://leetcode.com/problems/lfu-cache
+*/
+
+public class LFUCache {
+    Dictionary<int, CacheElement> cache;
+    SortedDictionary<int, List<int>> freqKeysMap;
+    int capacity;
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new();
+        freqKeysMap = new();
+    }
+    
+    public int Get(int key) {
+        if(cache.ContainsKey(key)){
+            RemoveFreqKey(cache[key].Frequency, key);
+            AddFreqKey(++cache[key].Frequency, key);
+            return cache[key].Value;
+        }
+        return -1;
+    }
+    
+    public void Put(int key, int value) {
+        if(capacity == 0)
+            return;
+
+        if(cache.ContainsKey(key)){
+            RemoveFreqKey(cache[key].Frequency, key);
+            AddFreqKey(++cache[key].Frequency, key);
+            cache[key].Value = value;
+            return;
+        }
+
+        if(cache.Count < capacity) {
+            cache.Add(key, new CacheElement(key, value, 1));
+            AddFreqKey(1, key);
+            return;
+        }
+
+        var elementToRemove = freqKeysMap.First();
+        var keyToRemove = elementToRemove.Value[0];
+        cache.Remove(keyToRemove);
+        RemoveFreqKey(elementToRemove.Key, keyToRemove);
+        cache.Add(key, new CacheElement(key, value, 1));
+        AddFreqKey(1, key);
+    }
+    
+    private void RemoveFreqKey(int freq, int key){
+        if(freqKeysMap[freq].Count == 1){
+            freqKeysMap.Remove(freq);
+        }
+        else{
+            freqKeysMap[freq].Remove(key);
+        }
+    }
+
+    private void AddFreqKey(int freq, int key){
+        if(freqKeysMap.ContainsKey(freq)){
+            freqKeysMap[freq].Add(key);
+        }
+        else{
+            freqKeysMap.Add(freq, new List<int>(){ key });
+        }
+    }
+}
+
+public class CacheElement{
+    public int Key;
+    public int Value;
+    public int Frequency;
+
+    public CacheElement(int key, int value, int frequency){
+        Key = key;
+        Value = value;
+        Frequency = frequency;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        LFUCache lFUCache = new(2);
+    }
+}
